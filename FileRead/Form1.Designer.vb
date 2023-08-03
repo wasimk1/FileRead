6c8a945cddd8b4402204cc0ce8125c4cfb9902a6
Wasim Khan <wasimkhan0816@gmail.com>
2023-08-03 21:01:42 +0530

    Added all the source file and folders related to it

---
+++ FileRead/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(158, 260)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(110, 23)
+        Me.btnBrowse.TabIndex = 0
+        Me.btnBrowse.Text = "Browse File"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Keyword 1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList
+        Me.TextBox1.Location = New System.Drawing.Point(158, 66)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(371, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Keyword 2"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(158, 128)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(371, 22)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(319, 260)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Submit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(158, 215)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(486, 22)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(78, 220)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Filename"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(674, 214)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(110, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Clear Result"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(613, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(52, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Found:"
+        Me.Label4.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(160, 93)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 17)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Label6"
+        Me.Label6.Visible = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(159, 157)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(51, 17)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Label7"
+        Me.Label7.Visible = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(671, 71)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(0, 17)
+        Me.Label8.TabIndex = 13
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(674, 133)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 17)
+        Me.Label9.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(613, 128)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 17)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Found:"
+        Me.Label5.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(812, 215)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Clear All"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(158, 298)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(117, 23)
+        Me.Button4.TabIndex = 17
+        Me.Button4.Text = "Browse Folder"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(1071, 370)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Name = "Form1"
+        Me.Text = "File Read"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Public WithEvents TextBox1 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ FileRead/Form1.vb
@@ -0,0 +1,149 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Dim fileReader As StreamReader
+    Dim fileWriter As StreamWriter
+    Dim getFileName As String
+    Dim writingfile As String
+    Dim readingfile As String
+    Dim keyword1 As String
+    Dim keyword2 As String
+    Dim d As Date = Date.Today
+    Dim updatedfilename As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Browsefile()
+
+    End Sub
+    Public Sub Browsefile()
+        Dim dialogbox As New OpenFileDialog()
+
+        dialogbox.Title = "Select File"
+        dialogbox.InitialDirectory = "E:\"
+        dialogbox.Filter = "All files(*.*)|*.*|All files(*.*)|*.*"
+        dialogbox.FilterIndex = 2
+        dialogbox.RestoreDirectory = True
+        If DialogResult.OK = dialogbox.ShowDialog Then
+            getFileName = dialogbox.FileName
+            TextBox3.Text = dialogbox.FileName
+            btnBrowse.Enabled = False
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ReadFile()
+    End Sub
+    Public Sub ReadFile()
+        If TextBox3.Text = "" Then
+            MessageBox.Show("Please browse the file")
+            Exit Sub
+        End If
+        updatedfilename = getFileName.Substring(3)
+        fileReader = My.Computer.FileSystem.OpenTextFileReader(getFileName)
+        readingfile = fileReader.ReadToEnd()
+        fileReader.Close()
+        fileWriter = My.Computer.FileSystem.OpenTextFileWriter("E:\result.csv", True)
+
+        keyword1 = TextBox1.Text
+        keyword2 = TextBox2.Text
+        If TextBox1.Text = "" And TextBox2.Text = "" Then
+            MessageBox.Show("Nothing Found!, Please enter keyword")
+        End If
+        If TextBox1.Text <> "" Then
+            If readingfile.Contains(keyword1) Then
+                Label4.Visible = True
+                Label8.Text = keyword1
+                fileWriter.WriteLine(Environment.NewLine + "," + keyword1 + "," + updatedfilename + "," + d.Date + "," + Now.ToLongTimeString)
+            Else
+                Label6.Visible = True
+                Label6.Text = "Keyword 1 not found"
+            End If
+        End If
+        If TextBox2.Text <> "" Then
+            If readingfile.Contains(keyword2) Then
+                Label5.Visible = True
+                Label9.Text = keyword2
+                fileWriter.WriteLine(Environment.NewLine + "," + keyword2 + "," + updatedfilename + "," + d.Date + "," + Now.ToLongTimeString)
+            Else
+                Label7.Visible = True
+                Label7.Text = "Keyword 2 not found"
+            End If
+        End If
+        fileWriter.Close()
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        cleartext()
+    End Sub
+    Public Sub cleartext()
+        Label6.Text = ""
+        Label7.Text = ""
+        Label4.Text = ""
+        Label5.Text = ""
+        Label8.Text = ""
+        Label9.Text = ""
+        TextBox3.Text = ""
+        btnBrowse.Enabled = True
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        clearAll()
+    End Sub
+    Public Sub clearAll()
+        cleartext()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        browseFolder()
+    End Sub
+
+    Public Sub browseFolder()
+        Dim f As String
+        keyword1 = TextBox1.Text
+        keyword2 = TextBox2.Text
+        If TextBox1.Text = "" And TextBox2.Text = "" Then
+            MessageBox.Show("Nothing Found!, Please enter keyword")
+            Exit Sub
+        End If
+        Dim folderBrowseDialog As FolderBrowserDialog = New FolderBrowserDialog()
+        folderBrowseDialog.SelectedPath = "E:\vbtestproject\"
+        Dim driNFO As New DirectoryInfo(folderBrowseDialog.SelectedPath)
+        Dim txtFiles As FileInfo() = driNFO.GetFiles("*.txt")
+
+        For Each txt_file As FileInfo In txtFiles
+            f = txt_file.Name
+            'MessageBox.Show(f)
+            fileReader = My.Computer.FileSystem.OpenTextFileReader("E:\vbtestproject\" + f)
+            readingfile = fileReader.ReadToEnd()
+            fileReader.Close()
+            fileWriter = My.Computer.FileSystem.OpenTextFileWriter("E:\result.csv", True)
+            'MessageBox.Show(readingfile)
+
+
+            If TextBox1.Text <> "" Then
+                If readingfile.Contains(keyword1) Then
+                    Label4.Visible = True
+                    Label8.Text = keyword1
+                    fileWriter.WriteLine(Environment.NewLine + "," + keyword1 + "," + f + "," + d.Date + "," + Now.ToLongTimeString)
+
+                End If
+            End If
+            If TextBox2.Text <> "" Then
+                If readingfile.Contains(keyword2) Then
+                    Label5.Visible = True
+                    Label9.Text = keyword2
+                    fileWriter.WriteLine(Environment.NewLine + "," + keyword2 + "," + f + "," + d.Date + "," + Now.ToLongTimeString)
+
+                End If
+            End If
+            fileWriter.Close()
+
+        Next
+        MessageBox.Show("File Writing Done")
+        'Button2.Enabled = False
+    End Sub
+
+End Class
